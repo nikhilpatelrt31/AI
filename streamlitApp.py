220f43e872791046d7be6e35eedcfd8f55032dde
nikhilpatelrt31 <nikhilpatelrt31@gmail.com>
2025-04-06 15:14:54 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -12,7 +12,7 @@ st.title('Timelytics: Optimize your supply chain with advanced forecasting techn
 st.caption('Timelytics is an ensemble model that utilizes three powerful machine learning algorithms - XGBoost, Random Forests, and Support Vector Machines (SVM) - to accurately forecast Order to Delivery (OTD) times. By combining the strengths of these three algorithms, Timelytics provides a robust and reliable prediction of OTD times, helping businesses to optimize their supply chain operations.')
 st.caption('With Timelytics, businesses can identify potential bottlenecks and delays in their supply chain and take proactive measures to address them, reducing lead times and improving delivery times. The model utilizes historical data on order processing times, production lead times, shipping times, and other relevant variables to generate accurate forecasts of OTD times. These forecasts can be used to optimize inventory management, improve customer service, and increase overall efficiency in the supply chain.')
 
-modelfile = "voting_model.pkl"
+modelfile = "./voting_model.pkl"
 try:
     with open(modelfile, "rb") as f:
         voting_model = pickle.load(f)
