360e1a278a3c1643b2167245c349acb01a8186f8
nikhilpatelrt31 <nikhilpatelrt31@gmail.com>
2025-04-06 21:52:24 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -12,7 +12,7 @@ st.caption('Timelytics is an ensemble model that utilizes three powerful machine
            )
 st.caption('With Timelytics, businesses can identify potential bottlenecks and delays in their supply chain and take proactive measures to address them, reducing lead times and improving delivery times. The model utilizes historical data on order processing times, production lead times, shipping times, and other relevant variables to generate accurate forecasts of OTD times. These forecasts can be used to optimize inventory management, improve customer service, and increase overall efficiency in the supply chain.'
            )
-modelfile = 'voting_model.pkl'
+modelfile = 'https://github.com/nikhilpatelrt31/AI/voting_model.pkl'
 voting_model = pickle.load(open(modelfile, 'rb'))
 
 # Caching the model for faster loading
