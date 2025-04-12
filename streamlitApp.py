963180c360cee61232060428a03c3da3aa50a9ec
nikhilpatelrt31 <nikhilpatelrt31@gmail.com>
2025-04-12 12:17:13 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 from PIL import Image
 st.set_page_config(page_title="Timelytics", page_icon=":pencil:", layout="wide")
-st.title("Timelytics: Optimize your supply chain with advanced forecasting techniques.")"
+st.title("Timelytics: Optimize your supply chain with advanced forecasting techniques.")
 st.caption("Timelytics is an ensemble model that utilizes three powerful machine learning algorithms - XGBoost, Random Forests, and Support Vector Machines (SVM) - to accurately forecast Order to Delivery (OTD) times. By combining the strengths of these three algorithms, Timelytics provides a robust and reliable prediction of OTD times, helping businesses to optimize their supply chain operations."
 )
 st.caption(
@@ -45,7 +45,7 @@ return round(prediction[0])
 with st.sidebar:
 img = Image.open("./assets/supply_chain_optimisation.jpg")
 st.image(img)
-st.header("Input Parameters")"
+st.header("Input Parameters")
 purchase_dow = st.number_input("Purchased Day of the Week", min_value=0, max_value=6, step=1, value=3)
 purchase_month = st.number_input("Purchased Month", min_value=1, max_value=12, step=1, value=1)
 year = st.number_input("Purchased Year", value=2018)
@@ -88,5 +88,5 @@ Distance": ["247.94", "250.35", "4.915"],
 # Create a DataFrame from the sample dataset.
 df = pd.DataFrame(data)
 # Display the sample dataset in the Streamlit app.
-"st.header("Sample Dataset")"
+st.header("Sample Dataset")
 st.write(df)
